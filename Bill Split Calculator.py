aafd5950251f3431d74bfc4085c022eca922843a
AndreTallens <andretallens@gmail.com>
2025-12-11 20:06:07 -0300

    refatoração do codigo, ajuste da variavel q calcula a divisao da conta por pessoa, alem de ajustar o print de forma a ficar mais organizado

--- Bill Split Calculator.py
+++ Bill Split Calculator.py
@@ -1,11 +1,11 @@
 print("Bill Split Calculator")
 bill_amount = float(input())
 tip_percentage = float(input())
+num_people = int(input())
 
 tip_amount = (tip_percentage / 100) * bill_amount
 total_amount = bill_amount + tip_amount
+amount_per_person = total_amount / num_people
 
-print(total_amount)
-
-people = int(input())
-print (total_amount / people)
+print(f"Total (including tip): ${total_amount}")
+print (f"Each person pays: ${amount_per_person}")
