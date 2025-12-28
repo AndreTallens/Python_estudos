9b6070615abc57008a119c6b4aa645daac2113fd
AndreTallens <andretallens@gmail.com>
2025-12-28 19:41:10 -0300

    argumentos

---
+++ argumentos.py
@@ -0,0 +1,9 @@
+# Declare your function here
+def mult(a, b):
+    print(a * b)
+# print the result of a*b inside the function
+
+a = int(input())
+b = int(input())
+# Call your function here with the arguments a and b
+mult(a, b)
