d4c4aff5118ba7a473b075167f122bd6f769b34a
AndreTallens <andretallens@gmail.com>
2025-12-29 22:32:02 -0300

    area triagulo

---
+++ desafio_Area_triangulo.py
@@ -0,0 +1,7 @@
+def calculate_area(length, width):
+    # Write your code below
+    print(length * width)
+length = float(input())
+width = float(input())
+# Call the function below
+calculate_area(length, width)
