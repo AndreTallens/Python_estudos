c1a1a8ef7316989379ecaa88f7cdcd1e093b05ba
AndreTallens <andretallens@gmail.com>
2026-01-09 20:18:55 -0300

    acessando elementos de uma lista

---
+++ acessando_elementos_da-lista.py
@@ -0,0 +1,9 @@
+def values(lst):
+    # Write code here
+    for i in range(len(lst)):
+        print(lst[i])
+#Crie uma função chamada values que recebe uma lista como argumento e imprime todos os itens da lista um após o outro.
+
+#Importante: A lista já está fornecida à função como o parâmetro lst. Você não precisa ler entrada ou criar a lista - ela já é passada como argumento. Você só precisa iterar através de lst e imprimir cada elemento.
+
+#Para iterar sobre uma lista, use a função len() dentro da função range():
---
+++ declaracao_lista.py
@@ -0,0 +1,10 @@
+#Uma lista é uma coleção de itens, e pode conter valores de diferentes tipos, como números, strings ou até outras listas. Listas são criadas usando colchetes [], e os itens dentro da lista são separados por vírgulas.
+
+#Aqui está um exemplo de como criar uma lista:
+
+my_list = [1, 2, "three", True]
+#Para verificar o comprimento da lista, podemos usar a função len():
+
+#length = len(my_list)
+#A variável length conterá 4 porque há 4 elementos na lista.
+
